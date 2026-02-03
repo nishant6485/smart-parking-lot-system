b0fadf072db82c2360da3d35148d130a4f75ece6
nishant6485 <nishant9027834292@gmail.com>
2026-02-03 19:37:10 +0530

    Implemented core features and logic

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ParkingLotSystem from './parking-lot-system.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ParkingLotSystem />
+  </React.StrictMode>
+);
